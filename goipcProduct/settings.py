044c11c8ed3a9d13aa655ba3e67f79d233e9eebe
chksong <chksong@qq.com>
2019-06-12 06:29:47 +0800

    使用mysql

--- goipcProduct/settings.py
+++ goipcProduct/settings.py
@@ -73,13 +73,26 @@ WSGI_APPLICATION = 'goipcProduct.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
+DATABASES = {
+     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'db_weixin',
+         'USER': 'weixin',
+         'PASSWORD': '123456',
+         'HOST': '127.0.0.1',
+         'PORT': '10002',
+         'OPTIONS': {
+              'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+          },
+      }
+ }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
